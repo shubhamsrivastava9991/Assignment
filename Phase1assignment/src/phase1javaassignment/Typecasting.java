1d2f803b4ff64d92bc6110421f5046ab54db6619
Shubham Srivastava <shubhamsrivastava9991@gmail.com>
2022-07-24 14:35:57 +0530

    Type casting assignment

---
+++ Phase1assignment/src/phase1javaassignment/Typecasting.java
@@ -0,0 +1,43 @@
+package phase1javaassignment;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		
+		//Conversion of float --> long data type 
+		
+		float f  = 25.678f;
+		long l = (long)f;  //explicit type casting
+		
+		System.out.println("The conversion of float datatype value of: " + f + " " + "to long datatype value is: " + l);
+		
+		//Conversion of double --> long --> --> int --> char data type
+		
+		double d = 89.564879;
+		long g = (long) d; //explicit type casting
+		int i = (int) g;  //explicit type casting
+		char c = (char) i; //explicit type casting
+		
+		System.out.println("The conversion of double datatype value of: " + d + " " + "to long datatype value is:" + g);
+		System.out.println("The conversion of long datatype value of: " + g + " " + "to int datatype value is: " + i);
+		System.out.println("The conversion of int datatype value of: " + i + " " + "to char datatype value is: " + c);
+		
+		//Conversion of long --> byte data type
+		
+		long h = 8954672;
+		byte b = (byte) h; //explicit type casting
+		
+		System.out.println("The conversion of long datatype value of: " + h + " " + "to byte datatype value is: " + b);
+		
+		//Conversion of int --> double --> long
+		
+		int j = 256;
+		double k = j; //implicit type casting
+		long n = (long) k; // explicit type casting
+		
+		System.out.println("The conversion of int datatype value of: " + j + " " + "to double datatype value is:" + k);
+		System.out.println("The conversion of double datatype value of: " + k + " " + "to long datatype value is:" + n);
+
+	}
+
+}
