c26bd9ece35eb06c4bbe1f2b03387bb0bf32d50b
Shubham Srivastava <shubhamsrivastava9991@gmail.com>
2022-07-24 14:16:52 +0530

    Adding Comment

--- Phase1assignment/src/phase1javaassignment/Areaofrectangle.java
+++ Phase1assignment/src/phase1javaassignment/Areaofrectangle.java
@@ -2,14 +2,17 @@ package phase1javaassignment;
 
 public class Areaofrectangle {
 	
+	//Method for calculating area 
 	int area(int l, int b){
 		return l*b;
 	}
 
 	public static void main(String[] args) {
 		
+		//Creating the object 
 		Areaofrectangle ar = new Areaofrectangle();
 		
+		//Calling the method and storing the return value to the variable 
 		int result = ar.area(25,15);
 		System.out.println("Area of Rectangle is: " + result);
 	}
